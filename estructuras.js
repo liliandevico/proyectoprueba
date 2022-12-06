17aea0b3166b14634a0bcf5f4ebd34177b743624
liliandevico <liliandevico@gmail.com>
2022-12-06 16:45:52 -0300

    probando

---
+++ estructuras.js
@@ -0,0 +1,22 @@
+// ARRAY
+let miArray = ['Star Wars', true, 23];
+
+//Funciones
+function calculadoraDeSaldos(a,b) {
+    
+}
+
+//FOR
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+//IF
+
+if (condition) {
+    
+} else {
+    
+}
+    
---
+++ tiposDeDatos.js
@@ -0,0 +1,50 @@
+//informacion sobre lo que estoy haciendo
+//Dato del tipo number
+let edad = 33;
+console.log(edad);
+
+//Dato del tipo numer con decimales
+let precio = 150.65;
+console.log(precio);
+
+//Dato del tipo String
+let nombre = "Lilian";
+console.log(nombre);
+
+//Datos del tipo string con comillas simples
+let apellido = 'Devico';
+console.log(apellido);
+
+// Datos Booleano con valor true
+let verdadero = true;
+console.log(verdadero);
+
+//Dato booleano con valor false
+let falso = false;
+console.log(falso);
+
+// Datos del tip not a number
+let notANumber = NaN;
+console.log(notANumber);
+
+// Datos vacio o desconocido Null
+let nulo = null;
+console.log(nulo);
+
+// Datos undefined
+let sinValor = "Sin Valor";
+console.log(sinValor)
+
+// Datos tipo objeto literal
+let persona = {
+    nombre: 'Lilian',
+    edad: 33
+}
+console.log(persona);
+
+//Dato tipo Array
+let paises = [ 'Argentina', 'Venezuela', 'Colombia' ];
+console.log(paises);
+
+
+
